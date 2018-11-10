39b0f4b451cb4b1efde055938b568da3a4adb7c8
Eric Myhre <hash@exultant.us>
2018-11-10 13:16:51 -0600

    Drop nonsense comments about RootNode.
    That subject has more to do with SerializableNode.  Which also might
    not be the name we end up with for that.  ID'able Node?  LinkableNode?
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- node.go
+++ node.go
@@ -6,12 +6,6 @@ type Node interface {
 	// GetField resolves a path in the the object and returns
 	// either a primitive (e.g. string, int, etc), a link (type CID),
 	// or another Node.
-	//
-	// If a Node is returned, it will be a unrooted node -- that is,
-	// it can be used to view the fields below it, but since it was not
-	// originally stored as a full node, you cannot immediately take
-	// a link to it for embedding in other objects (you'd have to make
-	// a new RootNode with the same content first, then store that).
 	TraverseField(path string) (Node, error)
 
 	// GetIndex is the equivalent of GetField but for indexing into an array
