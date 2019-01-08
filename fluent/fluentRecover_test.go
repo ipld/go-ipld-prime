caaf28f278ad77c8cc9c50b347f6cedabb27a67d
Eric Myhre <hash@exultant.us>
2019-01-08 15:54:33 +0100

    Impl and tests for fluent.Recover.
    Used the ipldfree.Node impl concretely to make the tests work.
    In theory any impl should of course work, but it's not useful or
    interesting to do a compatibility matrix here -- particularly given
    that we use the fluent system in the compataibility tests for all
    the other Node impl packages.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

---
+++ fluent/fluentRecover_test.go
@@ -0,0 +1,47 @@
+package fluent
+
+import (
+	"fmt"
+	"testing"
+
+	. "github.com/warpfork/go-wish"
+
+	"github.com/ipld/go-ipld-prime/impl/free"
+)
+
+func TestRecover(t *testing.T) {
+	t.Run("simple traversal error should capture", func(t *testing.T) {
+		Wish(t,
+			Recover(func() {
+				WrapNode(&ipldfree.Node{}).TraverseIndex(0).AsString()
+				t.Fatal("should not be reached")
+			}),
+			ShouldEqual,
+			Error{fmt.Errorf("cannot traverse a node that is undefined")},
+		)
+	})
+	t.Run("correct traversal should return nil", func(t *testing.T) {
+		Wish(t,
+			Recover(func() {
+				n := &ipldfree.Node{}
+				n.SetString("foo")
+				WrapNode(n).AsString()
+			}),
+			ShouldEqual,
+			nil,
+		)
+	})
+	t.Run("other panics should continue to rise", func(t *testing.T) {
+		Wish(t,
+			func() (r interface{}) {
+				defer func() { r = recover() }()
+				Recover(func() {
+					panic("fuqawds")
+				})
+				return
+			}(),
+			ShouldEqual,
+			"fuqawds",
+		)
+	})
+}
