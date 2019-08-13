61bf5b5d20c986497266b44d04a52f6eae574239
Eric Myhre <hash@exultant.us>
2019-08-13 23:15:42 +0200

    codegen: semantic builder for a struct got!
    And new tests.
    
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- schema/gen/go/_test/wow_test.go
+++ schema/gen/go/_test/wow_test.go
@@ -9,6 +9,7 @@ import (
 	ipld "github.com/ipld/go-ipld-prime"
 	"github.com/ipld/go-ipld-prime/encoding"
 	"github.com/ipld/go-ipld-prime/fluent"
+	ipldfree "github.com/ipld/go-ipld-prime/impl/free"
 )
 
 // TokenSourceBucket acts like a TokenSource by yielding tokens from a pre-made
@@ -24,6 +25,13 @@ func (tb *TokenSourceBucket) Step(yield *tok.Token) (done bool, err error) {
 	return tb.read > len(tb.tokens), nil
 }
 
+func plz(n ipld.Node, e error) ipld.Node {
+	if e != nil {
+		panic(e)
+	}
+	return n
+}
+
 func TestScalarUnmarshal(t *testing.T) {
 	t.Run("string node", func(t *testing.T) {
 		tb := &TokenSourceBucket{tokens: []tok.Token{
@@ -37,3 +45,47 @@ func TestScalarUnmarshal(t *testing.T) {
 		Wish(t, tb.read, ShouldEqual, 1)
 	})
 }
+
+// n.b. testing unmarshal is something different (this doesn't exercise
+// representation node/nodebuilder, just the semantic/type level one).
+func TestStructBuilder(t *testing.T) {
+	t.Run("stroct", func(t *testing.T) {
+		t.Run("all fields set", func(t *testing.T) {
+			mb, err := Stroct__NodeBuilder{}.CreateMap()
+			Require(t, err, ShouldEqual, nil)
+			mb.Insert(ipldfree.String("f1"), plz(String__NodeBuilder{}.CreateString("a")))
+			mb.Insert(ipldfree.String("f2"), plz(String__NodeBuilder{}.CreateString("b")))
+			mb.Insert(ipldfree.String("f3"), plz(String__NodeBuilder{}.CreateString("c")))
+			mb.Insert(ipldfree.String("f4"), plz(String__NodeBuilder{}.CreateString("d")))
+			n, err := mb.Build()
+
+			Wish(t, err, ShouldEqual, nil)
+			Wish(t, n.ReprKind(), ShouldEqual, ipld.ReprKind_Map)
+			Wish(t, plz(n.LookupString("f1")), ShouldEqual, plz(String__NodeBuilder{}.CreateString("a")))
+		})
+	})
+}
+
+/*
+soon...
+
+
+func TestStructUnmarshal(t *testing.T) {
+	t.Run("stroct", func(t *testing.T) {
+		t.Run("all fields set", func(t *testing.T) {
+			tb := &TokenSourceBucket{tokens: []tok.Token{
+				{Type: tok.TMapOpen, Length: 4},
+				{Type: tok.TString, Str: "f1"}, {Type: tok.TString, Str: "a"},
+				{Type: tok.TString, Str: "f2"}, {Type: tok.TString, Str: "b"},
+				{Type: tok.TString, Str: "f3"}, {Type: tok.TString, Str: "c"},
+				{Type: tok.TString, Str: "f4"}, {Type: tok.TString, Str: "d"},
+				{Type: tok.TMapClose},
+			}}
+			nb := Stroct__NodeBuilder{}
+			n, err := encoding.Unmarshal(nb, tb)
+			// ... asserts ...
+		})
+	})
+}
+
+*/
