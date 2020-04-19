25dcc61b1e465344b5b88578410559cad6656a01
Eric Myhre <hash@exultant.us>
2020-04-19 12:09:37 +0200

    Comments speculating on how to improve templates.
    The reuse level is currently *very* low.
    
    Some of that is intentional -- I'm writing radically non-DRY things
    on purpose in many cases, because I want to see what *actually*
    repeats most often before I start trying to remove repetitions.
    
    Some of it is not intentional; it's because I just have no idea
    how to organize units of reuse when doing templating.
    
    This latter group could improve.

--- schema/gen/go/templateUtil.go
+++ schema/gen/go/templateUtil.go
@@ -16,8 +16,27 @@ func doTemplate(tmplstr string, w io.Writer, adjCfg *AdjunctCfg, data interface{
 			"MaybeUsesPtr":     adjCfg.MaybeUsesPtr,
 			"add":              func(a, b int) int { return a + b },
 		}).
+		// Seriously consider prepending `{{ $dot := . }}` (or 'top', or something).
+		// Or a func into the map that causes `dot` to mean `func() interface{} { return data }`.
+		// The number of times that the range feature has a dummy capture line above it is... not reasonable.
+		//  (Grep for "/* ranging modifies dot, unhelpfully */" -- empirically, it's over 20 times already.)
 		Parse(wish.Dedent(tmplstr)))
 	if err := tmpl.Execute(w, data); err != nil {
 		panic(err)
 	}
 }
+
+// We really need to do some more composable stuff around here.
+// Generators should probably be carrying down their own doTemplate methods that curry customizations.
+// E.g., map generators would benefit hugely from being able to make a clause for "entTypeStrung", "mTypeStrung", etc.
+//
+// Open question: how exactly?  Should some of this stuff should be composed by:
+//   - composing template fragments;
+//   - amending the funcmap;
+//   - computing the whole result and injecting it as a string;
+//   - ... combinations of the above?
+// Adding to the complexity of the question is that sometimes we want to be
+//  doing composition inside the output (e.g. DRY by functions in the result,
+//   rather than by DRY'ing the templates).
+// Best practice to make this evolve nicely is not at all obvious to this author.
+//
