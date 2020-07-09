91923f8ab2f487b64622fedcb7e2789ae941ba35
Eric Myhre <hash@exultant.us>
2020-07-09 17:20:15 +0200

    Drop vestigial tests.
    These were apparently never used, or their users got churned under
    at some point and I failed to clean these up at the same time.
    
    It's a good idea to have tests that are agnostic to codegen vs
    runtime type semantics implementations... but at this point we should
    reengage with that by just extracting the stuff currently in the
    codegen package's tests, because it's much more comprehensive and
    also almost entirely abstracted already (it just happens to be in the
    wrong place in the file/package hierarchy for no particular reason).

--- schema/tests/doc.go
+++
@@ -1,4 +0,0 @@
-// The `schema/tests` package contains behavioral tests for type-constrained
-// Node implementations -- meant to work with either codegenerated Nodes OR
-// with the runtime schema.TypedNode wrappers, checking for the same behavior on each.
-package tests
