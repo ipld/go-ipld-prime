157fada43f36408e2ec9720eca82bb438161827a
Daniel Martí <mvdan@mvdan.cc>
2021-06-03 23:51:01 +0100

    node/tests: add more extensive scalar kind tests
    It covers AssignKind, AssignNode, and AsKind for every combination of
    assembler kind and method.
    
    We also verify that a constructed scalar node behaves the same with
    AsKind when using its representation, like the old test.
    
    There's effectively a triple loop as a test table, so the subtest name
    has up to three components separated by dashes, such as:
    
    	TestSchema/Scalars/Bytes-AssignNode-String
    
    We also use this test as a demo of quicktest instead of go-wish.
    
    Finally, adapt bindnode to pass these tests just like codegen. This was
    mainly a bunch of TODOs in the relevant methods.

---
+++ node/tests/schemaScalars.go
@@ -0,0 +1,126 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+
+	qt "github.com/frankban/quicktest"
+
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/schema"
+)
+
+func assignValue(am ipld.NodeAssembler, value interface{}) error {
+	switch value := value.(type) {
+	case bool:
+		return am.AssignBool(value)
+	case int64:
+		return am.AssignInt(value)
+	case float64:
+		return am.AssignFloat(value)
+	case string:
+		return am.AssignString(value)
+	case []byte:
+		return am.AssignBytes(value)
+	default:
+		panic(fmt.Sprintf("%T", value))
+	}
+}
+
+func SchemaTestScalars(t *testing.T, engine Engine) {
+	ts := schema.TypeSystem{}
+	ts.Init()
+
+	ts.Accumulate(schema.SpawnBool("Bool"))
+	ts.Accumulate(schema.SpawnInt("Int"))
+	ts.Accumulate(schema.SpawnFloat("Float"))
+	ts.Accumulate(schema.SpawnString("String"))
+	ts.Accumulate(schema.SpawnBytes("Bytes"))
+	engine.Init(t, ts)
+
+	var tests = []struct {
+		name  string
+		kind  ipld.Kind
+		value interface{}
+	}{
+		{"Bool", ipld.Kind_Bool, true},
+		{"Int", ipld.Kind_Int, int64(23)},
+		{"Float", ipld.Kind_Float, 12.25},
+		{"String", ipld.Kind_String, "foo"},
+		{"Bytes", ipld.Kind_Bytes, []byte("bar")},
+	}
+
+	// We test each of the five scalar prototypes in subtests.
+	for _, testProto := range tests {
+		np := engine.PrototypeByName(testProto.name)
+
+		// For each prototype, we try assigning all scalar values.
+		for _, testAssign := range tests {
+
+			// We try both AssignKind and AssignNode.
+			for _, useAssignNode := range []bool{false, true} {
+				testName := fmt.Sprintf("%s-Assign%s", testProto.name, testAssign.name)
+				if useAssignNode {
+					testName = fmt.Sprintf("%s-AssignNode-%s", testProto.name, testAssign.name)
+				}
+				t.Run(testName, func(t *testing.T) {
+					nb := np.NewBuilder()
+
+					// Assigning the right value for the kind should succeed.
+					var err error
+					if useAssignNode {
+						np2 := engine.PrototypeByName(testAssign.name)
+						nb2 := np2.NewBuilder()
+						qt.Assert(t, assignValue(nb2, testAssign.value), qt.IsNil)
+						n2 := nb2.Build()
+
+						err = nb.AssignNode(n2)
+					} else {
+						err = assignValue(nb, testAssign.value)
+					}
+					if testAssign.kind == testProto.kind {
+						qt.Assert(t, err, qt.IsNil)
+					} else {
+						qt.Assert(t, err, qt.Not(qt.IsNil))
+
+						// Assign something anyway, just so we can Build later.
+						err := assignValue(nb, testProto.value)
+						qt.Assert(t, err, qt.IsNil)
+					}
+
+					n := nb.Build()
+
+					// For both the regular node and its repr version,
+					// getting the right value for the kind should work.
+					for _, n := range []ipld.Node{
+						n,
+						n.(schema.TypedNode).Representation(),
+					} {
+						var gotValue interface{}
+						err = nil
+						switch testAssign.kind {
+						case ipld.Kind_Bool:
+							gotValue, err = n.AsBool()
+						case ipld.Kind_Int:
+							gotValue, err = n.AsInt()
+						case ipld.Kind_Float:
+							gotValue, err = n.AsFloat()
+						case ipld.Kind_String:
+							gotValue, err = n.AsString()
+						case ipld.Kind_Bytes:
+							gotValue, err = n.AsBytes()
+						default:
+							t.Fatal(testAssign.kind)
+						}
+						if testAssign.kind == testProto.kind {
+							qt.Assert(t, err, qt.IsNil)
+							qt.Assert(t, gotValue, qt.DeepEquals, testAssign.value)
+						} else {
+							qt.Assert(t, err, qt.Not(qt.IsNil))
+						}
+					}
+				})
+			}
+		}
+	}
+}
