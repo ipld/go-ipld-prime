62799630f980bb1c698dd9b982a041a208d27edf
Daniel Martí <mvdan@mvdan.cc>
2021-06-03 11:30:27 +0100

    node/bindnode: start running all schema tests
    We add node/tests.SchemaTestAll to simplify this task, meaning we don't
    need to duplicate all test func declarations in node/bindnode.
    SchemaTestAll is also flexible enough to allow running multiple
    sub-tests per schema test in the future.
    
    There were two remaining places in node/tests that still weren't using
    ipld.DeepEqual, so fix those.
    
    Finally, bindnode needed a couple of changes to fully support
    ipld.DeepEqual. Most notable is iteration over maps, which required a
    bit of a refactor to keep ordered keys.

---
+++ node/bindnode/schema_test.go
@@ -0,0 +1,52 @@
+package bindnode_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/ipld/go-ipld-prime"
+	"github.com/ipld/go-ipld-prime/node/bindnode"
+	"github.com/ipld/go-ipld-prime/node/tests"
+	"github.com/ipld/go-ipld-prime/schema"
+)
+
+// For now, we simply run all schema tests with PrototypeOnlySchema.
+// In the future, forSchemaTest might return multiple engines.
+
+func forSchemaTest(name string) []tests.EngineSubtest {
+	return []tests.EngineSubtest{{
+		Engine: &bindEngine{},
+	}}
+}
+
+func TestSchema(t *testing.T) {
+	t.Parallel()
+
+	tests.SchemaTestAll(t, forSchemaTest)
+}
+
+var _ tests.Engine = (*bindEngine)(nil)
+
+type bindEngine struct {
+	ts schema.TypeSystem
+}
+
+func (e *bindEngine) Init(t *testing.T, ts schema.TypeSystem) {
+	e.ts = ts
+}
+
+func (e *bindEngine) PrototypeByName(name string) ipld.NodePrototype {
+	wantRepr := strings.HasSuffix(name, ".Repr")
+	if wantRepr {
+		name = strings.TrimSuffix(name, ".Repr")
+	}
+	schemaType := e.ts.TypeByName(name)
+	if schemaType == nil {
+		return nil
+	}
+	proto := bindnode.PrototypeOnlySchema(schemaType)
+	if wantRepr {
+		proto = proto.(bindnode.TypedPrototype).Representation()
+	}
+	return proto
+}
