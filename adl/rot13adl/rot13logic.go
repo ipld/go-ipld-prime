6de08f70dc02cb6191728875907a685939c15184
Will Scott <will@cypherpunk.email>
2021-08-13 01:25:23 -0700

    use map rather than replacer

--- adl/rot13adl/rot13logic.go
+++ adl/rot13adl/rot13logic.go
@@ -4,75 +4,29 @@ import (
 	"strings"
 )
 
-var replaceTable = []string{
-	"A", "N",
-	"B", "O",
-	"C", "P",
-	"D", "Q",
-	"E", "R",
-	"F", "S",
-	"G", "T",
-	"H", "U",
-	"I", "V",
-	"J", "W",
-	"K", "X",
-	"L", "Y",
-	"M", "Z",
-	"N", "A",
-	"O", "B",
-	"P", "C",
-	"Q", "D",
-	"R", "E",
-	"S", "F",
-	"T", "G",
-	"U", "H",
-	"V", "I",
-	"W", "J",
-	"X", "K",
-	"Y", "L",
-	"Z", "M",
-	"a", "n",
-	"b", "o",
-	"c", "p",
-	"d", "q",
-	"e", "r",
-	"f", "s",
-	"g", "t",
-	"h", "u",
-	"i", "v",
-	"j", "w",
-	"k", "x",
-	"l", "y",
-	"m", "z",
-	"n", "a",
-	"o", "b",
-	"p", "c",
-	"q", "d",
-	"r", "e",
-	"s", "f",
-	"t", "g",
-	"u", "h",
-	"v", "i",
-	"w", "j",
-	"x", "k",
-	"y", "l",
-	"z", "m",
-}
-var unreplaceTable = func() []string {
-	v := make([]string, len(replaceTable))
-	for i := 0; i < len(replaceTable); i += 2 {
-		v[i] = replaceTable[i+1]
-		v[i+1] = replaceTable[i]
+func rot13(r rune) rune {
+	if r >= 'a' && r <= 'z' {
+		if r >= 'm' {
+			return r - 13
+		} else {
+			return r + 13
+		}
+	} else if r >= 'A' && r <= 'Z' {
+		if r >= 'M' {
+			return r - 13
+		} else {
+			return r + 13
+		}
 	}
-	return v
-}()
+	return r
+}
 
 // rotate transforms from the logical content to the raw content.
 func rotate(s string) string {
-	return strings.NewReplacer(replaceTable...).Replace(s)
+	return strings.Map(rot13, s)
 }
 
 // unrotate transforms from the raw content to the logical content.
 func unrotate(s string) string {
-	return strings.NewReplacer(unreplaceTable...).Replace(s)
+	return strings.Map(rot13, s)
 }
