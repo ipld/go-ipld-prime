5158220542955022d5a56bf749a600b5509b6f99
Daniel Martí <mvdan@mvdan.cc>
2021-09-10 15:34:49 +0200

    schema/dmt: sync with schema-schema changes, finish Compile
    Update the submodule and start using it in the schemadmt Parse test.
    Update the manually spawned schema-schema TypeSystem,
    and re-generate the types.go file.
    
    Note that our schema-schema TypeSystem uses optional fields
    for fields which the spec defines as "implicit false".
    This is because our schema package doesn't support implicits yet.
    Luckily, an optional bool is functionally very similar.
    
    Finally, nearly finish the Compile API.
    It works around the schema package's lack of inline types
    by accumulating types such as List__Foo and Map__Foo__Bar.
    Those are TODOs to be removed sooner than later.
    
    Finally, note that many repr strategies are TODOs for now.
    Future PRs will finish those bits of code.

--- schema/dmt/build.go
+++ schema/dmt/build.go
@@ -11,59 +11,21 @@ import (
 // Note that this API is EXPERIMENTAL and will likely change.
 // It is also unfinished and buggy.
 func Compile(ts *schema.TypeSystem, node *Schema) error {
+	ts.Accumulate(schema.SpawnBool("Bool"))
+	ts.Accumulate(schema.SpawnInt("Int"))
+	ts.Accumulate(schema.SpawnFloat("Float"))
+	ts.Accumulate(schema.SpawnString("String"))
+	ts.Accumulate(schema.SpawnBytes("Bytes"))
 	for _, name := range node.Types.Keys {
 		defn := node.Types.Values[name]
 
 		name := schema.TypeName(name)
-		switch {
-		// Scalar types without parameters.
-		case defn.TypeBool != nil:
-			ts.Accumulate(schema.SpawnBool(name))
-		case defn.TypeString != nil:
-			ts.Accumulate(schema.SpawnString(name))
-		case defn.TypeBytes != nil:
-			ts.Accumulate(schema.SpawnBytes(name))
-		case defn.TypeInt != nil:
-			ts.Accumulate(schema.SpawnInt(name))
-		case defn.TypeFloat != nil:
-			ts.Accumulate(schema.SpawnFloat(name))
-
-		case defn.TypeMap != nil:
-			typ := defn.TypeMap
-			if typ.ValueType.InlineDefn != nil {
-				return fmt.Errorf("TODO: support anonymous types in schema package")
-			}
-			switch {
-			case typ.Representation.MapRepresentation_Map != nil:
-				// default behavior
-			default:
-				return fmt.Errorf("TODO: support non-default map repr in schema package")
-			}
-			ts.Accumulate(schema.SpawnMap(name,
-				schema.TypeName(typ.KeyType),
-				schema.TypeName(*typ.ValueType.TypeName),
-				typ.ValueNullable,
-			))
-		case defn.TypeStruct != nil:
-			typ := defn.TypeStruct
-			var fields []schema.StructField
-			for _, fname := range typ.Fields.Keys {
-				field := typ.Fields.Values[fname]
-				if field.Type.InlineDefn != nil {
-					return fmt.Errorf("TODO: support anonymous types in schema package")
-				}
-				fields = append(fields, schema.SpawnStructField(fname,
-					schema.TypeName(*field.Type.TypeName),
-					field.Optional, field.Nullable,
-				))
-			}
-			ts.Accumulate(schema.SpawnStruct(name,
-				fields,
-				nil, // TODO
-			))
-		// TODO: unions etc
-		default:
+		// TODO: once we support anon types, remove the ts argument.
+		typ, err := spawnType(ts, name, defn)
+		if err != nil {
+			return err
 		}
+		ts.Accumulate(typ)
 	}
 
 	if errs := ts.ValidateGraph(); errs != nil {
@@ -72,4 +34,134 @@ func Compile(ts *schema.TypeSystem, node *Schema) error {
 		}
 	}
 	return nil
+}
+
+func todoFromImplicitlyFalseBool(b *bool) bool {
+	if b == nil {
+		return false
+	}
+	return *b
+}
+
+func todoAnonTypeName(nameOrDefn TypeNameOrInlineDefn) string {
+	if nameOrDefn.TypeName != nil {
+		return *nameOrDefn.TypeName
+	}
+	defn := *nameOrDefn.InlineDefn
+	switch {
+	case defn.TypeDefnMap != nil:
+		defn := defn.TypeDefnMap
+		return fmt.Sprintf("Map__%s__%s", defn.KeyType, todoAnonTypeName(defn.ValueType))
+	case defn.TypeDefnList != nil:
+		defn := defn.TypeDefnList
+		return fmt.Sprintf("List__%s", todoAnonTypeName(defn.ValueType))
+	default:
+		panic(fmt.Errorf("%#v", defn))
+	}
+}
+
+func spawnType(ts *schema.TypeSystem, name schema.TypeName, defn TypeDefn) (schema.Type, error) {
+	switch {
+	// Scalar types without parameters.
+	case defn.TypeDefnBool != nil:
+		return schema.SpawnBool(name), nil
+	case defn.TypeDefnString != nil:
+		return schema.SpawnString(name), nil
+	case defn.TypeDefnBytes != nil:
+		return schema.SpawnBytes(name), nil
+	case defn.TypeDefnInt != nil:
+		return schema.SpawnInt(name), nil
+	case defn.TypeDefnFloat != nil:
+		return schema.SpawnFloat(name), nil
+
+	case defn.TypeDefnList != nil:
+		typ := defn.TypeDefnList
+		if typ.ValueType.InlineDefn != nil {
+			return nil, fmt.Errorf("TODO: support anonymous types in schema package")
+		}
+		switch {
+		case typ.Representation == nil ||
+			typ.Representation.ListRepresentation_List != nil:
+			// default behavior
+		default:
+			return nil, fmt.Errorf("TODO: support non-default map repr in schema package")
+		}
+		return schema.SpawnList(name,
+			schema.TypeName(*typ.ValueType.TypeName),
+			todoFromImplicitlyFalseBool(typ.ValueNullable),
+		), nil
+	case defn.TypeDefnMap != nil:
+		typ := defn.TypeDefnMap
+		if typ.ValueType.InlineDefn != nil {
+			return nil, fmt.Errorf("TODO: support anonymous types in schema package")
+		}
+		switch {
+		case typ.Representation == nil ||
+			typ.Representation.MapRepresentation_Map != nil:
+			// default behavior
+		default:
+			return nil, fmt.Errorf("TODO: support non-default map repr in schema package")
+		}
+		return schema.SpawnMap(name,
+			schema.TypeName(typ.KeyType),
+			schema.TypeName(*typ.ValueType.TypeName),
+			todoFromImplicitlyFalseBool(typ.ValueNullable),
+		), nil
+	case defn.TypeDefnStruct != nil:
+		typ := defn.TypeDefnStruct
+		var fields []schema.StructField
+		for _, fname := range typ.Fields.Keys {
+			field := typ.Fields.Values[fname]
+			var typeName schema.TypeName
+			if field.Type.TypeName != nil {
+				typeName = schema.TypeName(*field.Type.TypeName)
+			} else if tname := todoAnonTypeName(field.Type); ts.TypeByName(tname) == nil {
+				typeName = schema.TypeName(tname)
+				// Note that TypeDefn and InlineDefn aren't the same enum.
+				anonDefn := TypeDefn{
+					TypeDefnMap:  field.Type.InlineDefn.TypeDefnMap,
+					TypeDefnList: field.Type.InlineDefn.TypeDefnList,
+					TypeDefnLink: field.Type.InlineDefn.TypeDefnLink,
+				}
+				anonType, err := spawnType(ts, typeName, anonDefn)
+				if err != nil {
+					return nil, err
+				}
+				ts.Accumulate(anonType)
+			} else {
+				typeName = schema.TypeName(tname)
+			}
+			fields = append(fields, schema.SpawnStructField(fname,
+				schema.TypeName(typeName),
+				todoFromImplicitlyFalseBool(field.Optional),
+				todoFromImplicitlyFalseBool(field.Nullable),
+			))
+		}
+		return schema.SpawnStruct(name,
+			fields,
+			nil, // TODO: struct repr
+		), nil
+	case defn.TypeDefnUnion != nil:
+		typ := defn.TypeDefnUnion
+		var members []schema.TypeName
+		for _, member := range typ.Members {
+			if member.TypeName != nil {
+				members = append(members, schema.TypeName(*member.TypeName))
+			} else {
+				panic("TODO: inline union members")
+			}
+		}
+		return schema.SpawnUnion(name,
+			members,
+			nil, // TODO: union repr
+		), nil
+	case defn.TypeDefnEnum != nil:
+		typ := defn.TypeDefnEnum
+		return schema.SpawnEnum(name,
+			typ.Members,
+			nil, // TODO: enum repr
+		), nil
+	default:
+		panic(fmt.Errorf("%#v", defn))
+	}
 }
