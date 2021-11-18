77c4f6db146d65e8a2cbcbd74ff15fcacadb66ca
Marco Munizaga <git@marcopolo.io>
2021-11-18 14:53:09 -0800

    Add toInterfaceValue

---
+++ fluent/toInterfaceValue.go
@@ -0,0 +1,67 @@
+package fluent
+
+import (
+	"github.com/ipld/go-ipld-prime/datamodel"
+)
+
+func ToInterfaceValue(node datamodel.Node) (interface{}, error) {
+	switch k := node.Kind(); k {
+	case datamodel.Kind_Invalid:
+		panic("invalid node")
+	case datamodel.Kind_Null:
+		return nil, nil
+	case datamodel.Kind_Bool:
+		return node.AsBool()
+	case datamodel.Kind_Int:
+		return node.AsInt()
+	case datamodel.Kind_Float:
+		return node.AsFloat()
+	case datamodel.Kind_String:
+		return node.AsString()
+	case datamodel.Kind_Bytes:
+		return node.AsBytes()
+	case datamodel.Kind_Link:
+		return node.AsLink()
+	case datamodel.Kind_Map:
+		outMap := make(map[interface{}]interface{})
+		for mi := node.MapIterator(); !mi.Done(); {
+			k, v, err := mi.Next()
+			if err != nil {
+				return nil, err
+			}
+			kVal, err := ToInterfaceValue(k)
+			if err != nil {
+				return nil, err
+			}
+			vVal, err := ToInterfaceValue(v)
+			if err != nil {
+				return nil, err
+			}
+			outMap[kVal] = vVal
+
+			if mi.Done() {
+				break
+			}
+		}
+		return outMap, nil
+	case datamodel.Kind_List:
+		outList := make([]interface{}, 0, node.Length())
+		for li := node.ListIterator(); !li.Done(); {
+			_, v, err := li.Next()
+			if err != nil {
+				return nil, err
+			}
+			vVal, err := ToInterfaceValue(v)
+			if err != nil {
+				return nil, err
+			}
+			outList = append(outList, vVal)
+
+			if li.Done() {
+				break
+			}
+		}
+		return outList, nil
+	}
+	panic("unhandled case in switch")
+}
---
+++ fluent/toInterfaceValue_test.go
@@ -0,0 +1,54 @@
+package fluent_test
+
+import (
+	"testing"
+
+	"github.com/ipld/go-ipld-prime/fluent"
+	"github.com/ipld/go-ipld-prime/node/basicnode"
+)
+
+func TestListValue(t *testing.T) {
+	a := []string{"a", "b", "c"}
+	n, err := fluent.Reflect(basicnode.Prototype.Any, a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out, err := fluent.ToInterfaceValue(n)
+	if err != nil {
+		t.Fatal(err)
+	}
+	outArr := out.([]interface{})
+
+	if len(a) != len(outArr) {
+		t.Errorf("Mismatch in array size")
+	}
+
+	for i, v := range outArr {
+		if a[i] != v {
+			t.Errorf("expected %v, got %v at index %v", a[i], v, i)
+		}
+	}
+}
+
+func TestMapValue(t *testing.T) {
+	a := map[string]interface{}{"a": "1", "b": int64(2), "c": 3.14}
+	n, err := fluent.Reflect(basicnode.Prototype.Any, a)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out, err := fluent.ToInterfaceValue(n)
+	if err != nil {
+		t.Fatal(err)
+	}
+	outM := out.(map[interface{}]interface{})
+
+	if len(a) != len(outM) {
+		t.Errorf("Mismatch in size")
+	}
+
+	for k, v := range outM {
+		if v != a[k.(string)] {
+			t.Errorf("expected %v, got %v at key %v", a[k.(string)], v, k)
+		}
+	}
+}
