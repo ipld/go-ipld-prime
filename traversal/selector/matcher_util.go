e0f4c23806e3f65a03530a55db5c4f7040dce062
Will Scott <will.scott@protocol.ai>
2022-03-03 16:00:28 +0100

    warning on readerat

--- traversal/selector/matcher_util.go
+++ traversal/selector/matcher_util.go
@@ -6,6 +6,10 @@ type readerat struct {
 	io.ReadSeeker
 }
 
+// ReadAt provides the io.ReadAt method over a ReadSeeker.
+// This implementation does not support concurrent calls to `ReadAt`,
+// as specified by the ReaderAt interface, and so must only be used
+// in non-concurrent use cases.
 func (r readerat) ReadAt(p []byte, off int64) (n int, err error) {
 	// TODO: consider keeping track of current offset.
 	_, err = r.Seek(off, 0)
