e0ee88fcbaf63e2d1064d5e4ec9de421a33b1810
Rod Vagg <rod@vagg.org>
2023-03-28 13:52:21 +1100

    doc: clean up and expand on traversal pkg docs

--- traversal/fns.go
+++ traversal/fns.go
@@ -26,9 +26,12 @@ type AdvVisitFn func(Progress, datamodel.Node, VisitReason) error
 type VisitReason byte
 
 const (
-	VisitReason_SelectionMatch     VisitReason = 'm' // Tells AdvVisitFn that this node was explicitly selected.  (This is the set of nodes that VisitFn is called for.)
-	VisitReason_SelectionParent    VisitReason = 'p' // Tells AdvVisitFn that this node is a parent of one that will be explicitly selected.  (These calls only happen if the feature is enabled -- enabling parent detection requires a different algorithm and adds some overhead.)
-	VisitReason_SelectionCandidate VisitReason = 'x' // Tells AdvVisitFn that this node was visited while searching for selection matches.  It is not necessarily implied that any explicit match will be a child of this node; only that we had to consider it.  (Merkle-proofs generally need to include any node in this group.)
+	// VisitReason_SelectionMatch tells AdvVisitFn that this node was explicitly selected.  (This is the set of nodes that VisitFn is called for.)
+	VisitReason_SelectionMatch VisitReason = 'm'
+	// VisitReason_SelectionParent tells AdvVisitFn that this node is a parent of one that will be explicitly selected.  (These calls only happen if the feature is enabled -- enabling parent detection requires a different algorithm and adds some overhead.)
+	VisitReason_SelectionParent VisitReason = 'p'
+	// VisitReason_SelectionCandidate tells AdvVisitFn that this node was visited while searching for selection matches.  It is not necessarily implied that any explicit match will be a child of this node; only that we had to consider it.  (Merkle-proofs generally need to include any node in this group.)
+	VisitReason_SelectionCandidate VisitReason = 'x'
 )
 
 // Progress tracks a traversal as it proceeds. It is used initially to begin a traversal, and it is then passed to the visit function as the traversal proceeds.
@@ -46,25 +49,56 @@ const (
 // Currently a best-guess approach is used to try and have the preloader adhere to the budget, but with typical real-world graphs, this is likely to be inaccurate.
 // In the case of inaccuracies, the budget will be properly applied to the traversal-proper, but the preloader may receive a different set of links than the traversal-proper will.
 type Progress struct {
-	Cfg       *Config
-	Path      datamodel.Path // Path is how we reached the current point in the traversal.
-	LastBlock struct {       // LastBlock stores the Path and Link of the last block edge we had to load.  (It will always be zero in traversals with no linkloader.)
+	// Cfg is the configuration for the traversal, set by user.
+	Cfg *Config
+
+	// Budget, if present, tracks "budgets" for how many more steps we're willing to take before we should halt.
+	// Budget is initially set by user, but is then updated as the traversal proceeds.
+	Budget *Budget
+
+	// Path is how we reached the current point in the traversal.
+	Path datamodel.Path
+
+	// LastBlock stores the Path and Link of the last block edge we had to load.  (It will always be zero in traversals with no linkloader.)
+	LastBlock struct {
 		Path datamodel.Path
 		Link datamodel.Link
 	}
-	PastStartAtPath bool                        // Indicates whether the traversal has progressed passed the StartAtPath in the config -- use to avoid path checks when inside a sub portion of a DAG that is entirely inside the "not-skipped" portion of a traversal
-	Budget          *Budget                     // If present, tracks "budgets" for how many more steps we're willing to take before we should halt.
-	SeenLinks       map[datamodel.Link]struct{} // Set used to remember which links have been visited before, if Cfg.LinkVisitOnlyOnce is true.
+
+	// PastStartAtPath indicates whether the traversal has progressed passed the StartAtPath in the config -- use to avoid path checks when inside a sub portion of a DAG that is entirely inside the "not-skipped" portion of a traversal
+	PastStartAtPath bool
+
+	// SeenLinks is a set used to remember which links have been visited before, if Cfg.LinkVisitOnlyOnce is true.
+	SeenLinks map[datamodel.Link]struct{}
 }
 
 // Config is a set of options for a traversal. Set a Config on a Progress to customize the traversal.
 type Config struct {
-	Ctx                            context.Context                // Context carried through a traversal.  Optional; use it if you need cancellation.
-	LinkSystem                     linking.LinkSystem             // LinkSystem used for automatic link loading, and also any storing if mutation features (e.g. traversal.Transform) are used.
-	LinkTargetNodePrototypeChooser LinkTargetNodePrototypeChooser // Chooser for Node implementations to produce during automatic link traversal.
-	LinkVisitOnlyOnce              bool                           // By default, we visit across links wherever we see them again, even if we've visited them before, because the reason for visiting might be different than it was before since we got to it via a different path.  If set to true, track links we've seen before in Progress.SeenLinks and do not visit them again.  Note that sufficiently complex selectors may require valid revisiting of some links, so setting this to true can change behavior noticably and should be done with care.
-	StartAtPath                    datamodel.Path                 // If set, causes a traversal to skip forward until passing this path, and only then begins calling visit functions.  Block loads will also be skipped wherever possible.
-	Preloader                      preload.Loader                 // Receives a list of links within each block prior to traversal-proper.	This can be used to asynchronously load blocks that will be required at a later phase of the retrieval, or even to load blocks in a different order than the traversal would otherwise do. Preload calls are not de-duplicated, it is up to the receiver to do so if desired. Beware of using both Budget and Preloader! See the documentation on Progress for more information.
+	// Ctx is the context carried through a traversal.
+	// Optional; use it if you need cancellation.
+	Ctx context.Context
+
+	// LinkSystem is used for automatic link loading, and also any storing if mutation features (e.g. traversal.Transform) are used.
+	LinkSystem linking.LinkSystem
+
+	// LinkTargetNodePrototypeChooser is a chooser for Node implementations to produce during automatic link traversal.
+	LinkTargetNodePrototypeChooser LinkTargetNodePrototypeChooser
+
+	// LinkVisitOnlyOnce controls repeat-link visitation.
+	// By default, we visit across links wherever we see them again, even if we've visited them before, because the reason for visiting might be different than it was before since we got to it via a different path.
+	// If set to true, track links we've seen before in Progress.SeenLinks and do not visit them again.
+	// Note that sufficiently complex selectors may require valid revisiting of some links, so setting this to true can change behavior noticably and should be done with care.
+	LinkVisitOnlyOnce bool
+
+	// StartAtPath, if set, causes a traversal to skip forward until passing this path, and only then begins calling visit functions.
+	// Block loads will also be skipped wherever possible.
+	StartAtPath datamodel.Path
+
+	// Preloader receives links within each block prior to traversal-proper by performing a lateral scan of a block without descending into links themselves before backing up and doing a traversal-proper.
+	// This can be used to asynchronously load blocks that will be required at a later phase of the retrieval, or even to load blocks in a different order than the traversal would otherwise do.
+	// Preload calls are not de-duplicated, it is up to the receiver to do so if desired.
+	// Beware of using both Budget and Preloader!  See the documentation on Progress for more information on this usage and the likely surprising effects.
+	Preloader preload.Loader
 }
 
 // Budget is a set of monotonically-decrementing "budgets" for how many more steps we're willing to take before we should halt.
@@ -75,9 +109,14 @@ type Config struct {
 // If you set any budgets (by having a non-nil Progress.Budget field), you must set some value for all of them.
 // Traversal halts when _any_ of the budgets reaches zero.
 // The max value of an int (math.MaxInt64) is acceptable for any budget you don't care about.
+//
+// Beware of using both Budget and Preloader!  See the documentation on Progress for more information on this usage and the likely surprising effects.
 type Budget struct {
-	NodeBudget int64 // A monotonically-decrementing "budget" for how many more nodes we're willing to visit before halting.
-	LinkBudget int64 // A monotonically-decrementing "budget" for how many more links we're willing to load before halting.  (This is not aware of any caching; it's purely in terms of links encountered and traversed.)
+	// NodeBudget is a monotonically-decrementing "budget" for how many more nodes we're willing to visit before halting.
+	NodeBudget int64
+	// LinkBudget is a monotonically-decrementing "budget" for how many more links we're willing to load before halting.
+	// (This is not aware of any caching; it's purely in terms of links encountered and traversed.)
+	LinkBudget int64
 }
 
 // Clone returns a copy of the budget.
